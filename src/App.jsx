9a5891204d57c1f02d4343fcc0d7a3cc14c0c5ef
Muyang Roshella <muyangroshella40@gmail.com>
2023-05-15 11:06:09 +0100

    Rosh

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+import './App.css';
+import Header from './components/header/Header'
+import Navbar from './components/navbar/Navbar'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonial from './components/testimonial/Testimonial'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
-export default App
+function App() {
+  return (
+  <>
+  <Header/>
+  <Navbar/>
+  <About/>
+  <Experience/>
+  <Services/>
+  <Portfolio/>
+  <Testimonial/>
+<Contact/>
+<Footer/>
+</>
+);
+}
+export default App;
