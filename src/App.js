5335c6979af47ec4184d7b631a8225af586dcb50
Muyang Roshella <muyangroshella40@gmail.com>
2023-06-06 03:59:44 +0100

    Replace old-folder with new-folder

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ import './App.css';
 // export default App;
 
 
-import Header from './components/header/Header';
-import Navbar from './components/navbar/Navbar';
-import About from './components/about/About';
+import Header from './components/header/ Header';
+import Navbar from './components/navbar/ Navbar';
+import About from './components/about/ About';
 import Experience from './components/experience/Experience';
-import Services from './components/services/Services';
-import Portfolio from './components/portfolio/Portfolio';
-import Testimonial from './components/testimonial/Testimonial';
+import Services from './components/services/ Services';
+import Portfolio from './components/portfolio/ Portfolio';
+import Testimonial from './components/testimonial/ Testimonial';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,32 @@
+//import "C:\Users\CONCEPTS\Documents\my-app\src\components\navbar\navbar.css"
 import React from 'react'
+import './navbar.css'
+import { AiOutlineHome } from "react-icons/ai"
+import { AiOutlineUser } from "react-icons/ai"
+import {BiBook } from 'react-icons/bi'
+import {RiServiceLine } from 'react-icons/ri'
+import { BiMessageSquareDetail } from "react-icons/bi"
+const Nav = () => {  
+return (
+    <nav className="navigation">
+      <a href="#Home">
+        <AiOutlineHome />
+      </a>
+      <a href="#about">
+        <AiOutlineUser />
+      </a>
+      <a href="#contact">
+        <BiMessageSquareDetail />
+      </a>
 
-
-export default function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+      <a href="#eperience">
+        <BiBook />
+      </a>
+      <a href="#services">
+        <RiServiceLine />
+      </a>
+    
+    </nav>
+  );
 }
+export default Nav
